fa428d7597303ab509f33705b8a9975102680567
Fuseji04 <fiw11za0802@gmail.com>
2023-09-28 10:43:49 +0700

    page147

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-                        // @ = เริ่มที่ src
-                        // ใช้componentไหนอย่าลืม!! import
 import HelloWorld from '@/components/HelloWorld'
 import Test from '@/components/Test'
 import UserIndex from '@/components/User/Index'
@@ -15,13 +13,11 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    // 1{} = 1 สมาชิกใน Array หรือ 1 route
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
     },
-    // http://localhost:8080/#/test
     {
       path: '/test',
       name: 'test',
@@ -38,12 +34,12 @@ export default new Router({
       component: UserCreate
     },
     {
-      path: '/user/edit',
+      path: '/user/edit/:userId',
       name: '/user-edit',
       component: UserEdit
     },
     {
-      path: '/user',
+      path: '/user/:userId',
       name: '/user',
       component: UserShow
     }
